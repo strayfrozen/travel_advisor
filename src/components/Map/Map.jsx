a99945fc2d21f021344ea7c80f88a0d5607c2c22
Stray <strayfrozen@yahoo.com>
2022-06-22 10:35:48 -0500

    added map styles and updated map jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import googleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from ',/styles';
 
 const Map = () => {
   return (
