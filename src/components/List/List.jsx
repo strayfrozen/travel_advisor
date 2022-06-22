4412390f925296806de1d739d23017556d854d2a
Stray <strayfrozen@yahoo.com>
2022-06-22 12:34:48 -0500

    updated list jsk

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core';
 
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+
 import useStyles from './styles'
 
 const List = () => {
@@ -11,8 +13,13 @@ const List = () => {
   const places = [
     {name: 'Cool Place'},
     {name: 'Best Beer'},
+    {name: 'Best Steak'},
+    {name: 'Cool Place'},
+    {name: 'Best Beer'},
+    {name: 'Best Steak'},
+    {name: 'Cool Place'},
+    {name: 'Best Beer'},
     {name: 'Best Steak'}
-
 ];
 
   return (
@@ -38,7 +45,7 @@ const List = () => {
       <Grid container spacing={3} className={classes.list}>
         {places?.map((place, i) => (
           <Grid item key={i} xs={12}>
-            <Card />
+            <PlaceDetails place={place} />
           </Grid>  
         ))}
       </Grid>
