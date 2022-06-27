c031ef0af17e99b21a34247bc601a3703bad0424
Stray <strayfrozen@yahoo.com>
2022-06-27 17:37:45 -0500

    updated App.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,31 @@
-/* eslint-disable consistent-return */
-import axios from 'axios';
+import axios from "axios";
 
-export const getPlacesData = async (type, sw, ne) => {
-  try {
-    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
-      headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-    return data;
-  } catch (error) {
-    console.log(error);
+const options = {
+  method: 'GET',
+  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    
+  },
+  headers: {
+    'X-RapidAPI-Key': 'b8f1602ee8mshf9e6fbc121b76a3p11a173jsnde75b992930f',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
   }
 };
 
-export const getWeatherData = async (lat, lng) => {
-  try {
-    if (lat && lng) {
-      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
-        params: { lat, lon: lng },
-        headers: {
-          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
-          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        },
-      });
+export const getPlacesData = async () => {
+    try{
+        const {data: {data} } = await axios.get(URL, options);
 
-      return data;
+        return data;
+    } catch (error) {
+        console.log(error)
+
     }
-  } catch (error) {
-    console.log(error);
-  }
-};
+}
+
