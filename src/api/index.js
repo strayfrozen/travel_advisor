7d1fc6b1877576c5b3f98cb663b3a13d8a9ea3f9
Stray <strayfrozen@yahoo.com>
2022-07-25 16:05:14 -0500

    updated header component

--- src/api/index.js
+++ src/api/index.js
@@ -2,25 +2,20 @@ import axios from "axios";
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-const options = {
-  method: 'GET',
-  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-    
-  },
-  headers: {
-    'X-RapidAPI-Key': 'b8f1602ee8mshf9e6fbc121b76a3p11a173jsnde75b992930f',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
     try{
-        const {data: {data} } = await axios.get(URL, options);
+        const {data: {data} } = await axios.get(URL, {
+            params: {
+              bl_latitude: sw.lat,
+              tr_latitude: ne.lat,
+              bl_longitude: sw.lng,
+              tr_longitude: ne.lng,
+            },
+            headers: {
+              'X-RapidAPI-Key': 'b8f1602ee8mshf9e6fbc121b76a3p11a173jsnde75b992930f',
+              'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+            }
+          }); 
 
         return data;
     } catch (error) {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,31 @@ import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles'
+import useStyles from './styles.js';
 
-const Header = () => {
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        <Typography varient="h5" className={classes.title}>
+        <Typography variant="h5" className={classes.title}>
           Travel Advisor
         </Typography>
         <Box display="flex">
-          <Typography varient="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          {/* <Autocomplete> */}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder="Search..." classes={{ root:classes.inputRoot, input: classes.inputInput}} />
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          {/* </Autocomplete> */}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
   );
-}
-
-export default Header
+};
